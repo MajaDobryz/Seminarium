d478a3b297eda5463175231714d2392fa97201c8
Maja <166402113+MajaDobryz@users.noreply.github.com>
2024-04-19 10:09:25 +0200

    Add multi route handler

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-var http = require('http')
+const http = require('http')
 
-http.createServer((req, res) => {
+const server = http.createServer(onRequest)
+
+function onRequest(req, res) {
+  if (req.url === '/') {
+    return getBase(req, res)
+  } else if (req.url === '/boop') {
+    return getBoop(req, res)
+  }
+
+  res.writeHead(404, {'Content-Type': 'text/plain'})
+  res.end('not found')
+}
+
+function getBase(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('hello')
+}
+
+function getBoop(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('boop the snoot')
-}).listen(8080)
+}
+
+server.listen(8080)
